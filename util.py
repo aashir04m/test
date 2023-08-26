67ed757b301f6b9a2d978061446499ee2896bbe9
aashir04m <108630620+aashir04m@users.noreply.github.com>
2023-08-26 23:55:26 +0500

    Add files via upload

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import base64
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
-
+import cv2
 
 def set_background(image_file):
     """
@@ -43,18 +43,18 @@ def classify(image, model):
         A tuple of the predicted class name and the confidence score for that prediction.
     """
     # convert image to (224, 224)
-    image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
+    # image = ImageOps.fit(image, (28, 28), Image.Resampling.LANCZOS)
 
-    # convert image to numpy array
-    image_array = np.asarray(image)
+    # # convert image to numpy array
+    # image_array = np.asarray(image)
 
-    # normalize image
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    # # normalize image
+    # normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-    # set model input
-    data = np.ndarray(shape=(1, 28, 28, 3), dtype=np.float32)
-    print("data", data)
-    data[0] = normalized_image_array
+    # # set model input
+    # data = np.ndarray(shape=(1, 28, 28, 3), dtype=np.float32)
+    # print("data", data)
+    # data[0] = normalized_image_array
 
     classes = {4: ('nv', ' melanocytic nevi'),
            6: ('mel', 'melanoma'),
@@ -64,18 +64,28 @@ def classify(image, model):
            0: ('akiec', 'Actinic keratoses and intraepithelial carcinomae'),
            3: ('df', 'dermatofibroma')}
     
-    # make prediction
-    prediction = model.predict(data)
-    print("prediction", prediction)
-    # index = np.argmax(prediction)
-    max_prob = max(prediction[0])
-    print("max_prob", max_prob)
-    class_ind = list(prediction[0]).index(max_prob)
-    print("class_ind", class_ind)
+    # # make prediction
+    # prediction = model.predict(data)
+    # print("prediction", prediction)
+    # # index = np.argmax(prediction)
+    # max_prob = max(prediction[0])
+    # print("max_prob", max_prob)
+    # class_ind = list(prediction[0]).index(max_prob)
+    # print("class_ind", class_ind)
+    # class_name = classes[class_ind]
+    # print("class_name", class_name)
+    # # index = 0 if prediction[0][0] > 0.95 else 1
+    # # class_name = class_names[index]
+    # # confidence_score = prediction[0][class_ind]
+
+    img = cv2.imread(image)
+    # cv2.imwrite(img)
+    cv2_imshow(img)
+    img = cv2.resize(img, (28, 28))
+    result = model.predict(img.reshape(1, 28, 28, 3))
+    max_prob = max(result[0])
+    class_ind = list(result[0]).index(max_prob)
     class_name = classes[class_ind]
-    print("class_name", class_name)
-    # index = 0 if prediction[0][0] > 0.95 else 1
-    # class_name = class_names[index]
-    # confidence_score = prediction[0][class_ind]
+    print(class_name)
 
     return class_name
